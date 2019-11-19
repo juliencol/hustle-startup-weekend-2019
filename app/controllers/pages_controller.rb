7acc177f032d9688f7ddc5d982270f7041540614
Julien Colombain <juliencol@macbook-pro-de-julien.home>
2019-11-19 02:16:30 +0100

    bug route

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,28 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def find
+  end
+
+  def brand
+  end
+
+  def sell
+  end
+
+  def test
+  end
+
+  def officehours
+  end
+
+  def feedback
+  end
+
+  def prototype
+  end
+
+  def pitch
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+
+  get 'pages/', to: 'pages#find', as: "find"
+  get 'pages/', to: 'pages#brand', as: "brand"
+  get 'pages/', to: 'pages#sell', as: "sell"
+  get 'pages/', to: 'pages#test', as: "test"
+  get 'pages/', to: 'pages#officehours', as: "officehours"
+  get 'pages/', to: 'pages#feedback', as: "feedback"
+  get 'pages/', to: 'pages#prototype', as: "prototype"
+  get 'pages/', to: 'pages#pitch', as: "pitch"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
