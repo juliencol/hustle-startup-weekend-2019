e07ec8f3d8268ac96b22ae19de0efd5d9e0052cb
Julien Colombain <juliencol@macbook-pro-de-julien.home>
2019-11-19 02:38:07 +0100

    proto done

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
-  get 'pages/', to: 'pages#find', as: "find"
-  get 'pages/', to: 'pages#brand', as: "brand"
-  get 'pages/', to: 'pages#sell', as: "sell"
-  get 'pages/', to: 'pages#test', as: "test"
-  get 'pages/', to: 'pages#officehours', as: "officehours"
-  get 'pages/', to: 'pages#feedback', as: "feedback"
-  get 'pages/', to: 'pages#prototype', as: "prototype"
-  get 'pages/', to: 'pages#pitch', as: "pitch"
+  get 'pages/find', to: 'pages#find', as: "find"
+  get 'pages/brand', to: 'pages#brand', as: "brand"
+  get 'pages/sell', to: 'pages#sell', as: "sell"
+  get 'pages/test', to: 'pages#test', as: "test"
+  get 'pages/officehours', to: 'pages#officehours', as: "officehours"
+  get 'pages/feedback', to: 'pages#feedback', as: "feedback"
+  get 'pages/prototype', to: 'pages#prototype', as: "prototype"
+  get 'pages/pitch', to: 'pages#pitch', as: "pitch"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
